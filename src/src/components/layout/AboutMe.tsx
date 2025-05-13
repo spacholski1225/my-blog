799c554a5e68f93fde7797c51dd9bda31faa2356
spacholski <spacholski99@gmail.com>
2025-05-13 15:50:22 +0200

    Changed twitter icon to the instagram

--- src/src/components/layout/AboutMe.tsx
+++ src/src/components/layout/AboutMe.tsx
@@ -38,12 +38,14 @@ const AboutMe = () => {
               </svg>
               LinkedIn
             </Link>
-            <Link href="https://twitter.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-button twitter">
-              {/* Twitter Icon */}
+            <Link href="https://instagram.com/yourusername" target="_blank" rel="noopener noreferrer" className="social-button instagram">
+              {/* Instagram Icon */}
               <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-                <path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-1-4.8 4-7.6 7.5-4.5.8-.5 1.5-1 2.5-1.5z"></path>
+                <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+                <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+                <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
               </svg>
-              Twitter
+              Instagram
             </Link>
           </div>
         </div>
